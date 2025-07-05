765bca882f23b353aaa3cafd66007a3b4369a15b
sociietyworld <shuiisociietyproductions@gmail.com>
2025-07-05 14:38:31 -0700

    Add full SOCIIETY homepage

---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+// Home Page Placeholder
+export default function Home() {
+  return <div className='text-white bg-black p-10'>SOCIIETY Homepage</div>
+}
---
+++ app/pledge/page.tsx
@@ -0,0 +1,4 @@
+// Pledge Form Placeholder
+export default function Pledge() {
+  return <div className='text-white bg-black p-10'>Join SOCIIETY Form</div>
+}
